db64de82d639051bcad33e1fd456edc109dacce1
Lucas Cunegundes <lucas.cunegundes@proxion.com.br>
2025-09-18 10:57:14 -0300

    feat(auth-middleware): criando arquitetura inicial e documento de requisitos - Dockerfile, .dockerignore e docker-compose - Documento de requisitos

---
+++ auth-middleware/src/main/java/com/moneyflow/auth_middleware/AuthMiddlewareApplication.java
@@ -0,0 +1,13 @@
+package com.moneyflow.auth_middleware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMiddlewareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMiddlewareApplication.class, args);
+	}
+
+}
